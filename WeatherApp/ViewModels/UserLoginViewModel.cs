a83e46678878bc4f92f794b4c98d1c932a789b11
Krzysztof Grzeslak <Krzysztof.grzeslak@WP.pl>
2018-06-10 00:52:34 +0200

    Cleanup

--- WeatherApp/ViewModels/UserLoginViewModel.cs
+++ WeatherApp/ViewModels/UserLoginViewModel.cs
@@ -12,7 +12,5 @@ namespace WeatherApp.ViewModels
         [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-
-        public string ValidationMessage { get; set; }
     }
 }
