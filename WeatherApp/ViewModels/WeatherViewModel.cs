634ea433420471c09224914df69a213cc9d4a55b
Krzysztof Grzeslak <Krzysztof.grzeslak@WP.pl>
2018-06-09 23:53:29 +0200

    Cleaned up bootstrap and added styles to forms

--- WeatherApp/ViewModels/WeatherViewModel.cs
+++ WeatherApp/ViewModels/WeatherViewModel.cs
@@ -1,13 +1,22 @@
-﻿namespace WeatherApp.ViewModels
+﻿using System.ComponentModel;
+
+namespace WeatherApp.ViewModels
 {
     public class WeatherViewModel
     {
         public int Id { get; set; }
-        public string Location { get; set; }
         public int CityId { get; set; }
+        public string Location { get; set; }
+
         public int Temperature { get; set; }
+
+        [DisplayName("Minimum")]
         public int MinTemperature { get; set; }
+
+        [DisplayName("Maximum")]
         public int MaxTemperature { get; set; }
+
+        [DisplayName("Atm. Pressure")]
         public int Pressure { get; set; }
         public int Humidity { get; set; }
     }
