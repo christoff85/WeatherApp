6c86abb0c9ce7a79354cce22523b5149bef15310
Krzysztof Grzeslak <Krzysztof.grzeslak@WP.pl>
2018-06-11 00:36:14 +0200

    Usings cleanup

--- WeatherApp.Tests/Providers/WeatherWebClient/OpenWeatherJsonDeserializerTests.cs
+++ WeatherApp.Tests/Providers/WeatherWebClient/OpenWeatherJsonDeserializerTests.cs
@@ -1,11 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NSubstitute;
-using WeatherApp.Domain.Models;
 using WeatherApp.Providers.Abstractions.WeatherWebClient;
 using WeatherApp.Providers.WeatherWebClient;
 
